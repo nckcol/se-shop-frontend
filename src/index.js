12b1d68e98d23493ce3ec314cea681d2f048dc07
Nikita Popov <nckcol@gmail.com>
2018-05-31 01:22:49 +0300

    feat: start project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+import './index.css';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
